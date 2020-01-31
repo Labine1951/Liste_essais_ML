3b55c952e19b41dd9f718c80b56365ef9503a11c
JAM Lefebvre <JAM6752@hotmail.com>
2020-01-31 18:50:23 -0500

    ajout de app2 et app3

---
+++ app2.js
@@ -0,0 +1,10 @@
+
+// app.js  sur github
+
+console.log(4 + 5, 'hello', true);
+
+let monMessage = "ca va bien aller";
+console.log(monMessage);
+console.log("ceci est le fichier app2.js");
+
+
---
+++ app3.js
@@ -0,0 +1,11 @@
+
+// app.js  sur github
+
+console.log(5 + 6, 'hello', true);
+
+let monMessage = "ca va bien aller";
+console.log(monMessage);
+console.log("ceci est le fichier app3.js");
+
+
+
